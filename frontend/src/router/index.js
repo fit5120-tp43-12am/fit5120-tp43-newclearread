b10255290f29590185057155a6657e5f12771184
aubreywang1025 <bwan0850@student.monash.edu>
2026-04-11 05:39:47 +1000

    Create the structure of Dyslexia page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import DyslexiaPage from '../views/DyslexiaPage.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
+  { path: '/dyslexia', name: 'Dyslexia', component: DyslexiaPage },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0 }
+  },
 })
 
 export default router
